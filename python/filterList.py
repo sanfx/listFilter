780b5346477dbee96dc692d4fcce989c25007ff6
Sanjeev <skysan@gmail.com>
2013-10-18 00:23:31 +0530

    remvoed usage of ast module, implemented using argparse instead of sys.argv and renamed Exception name

--- python/filterList.py
+++ python/filterList.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 
 import sys
-import ast
 import logging
 
 
-class ItemNotString(Exception):
+class FilterListException(Exception):
 	pass
 
 
@@ -23,7 +22,7 @@ class FilterList(object):
 			if type(item) == str:
 				continue
 			else:
-				raise ItemNotString("%i item '%s' is not of type string" % (index+1, item))
+				raise FilterListException("%i item '%s' is not of type string" % (index+1, item))
 		return True
 
 	def filterList(self, lst):
@@ -40,24 +39,16 @@ class FilterList(object):
 				return
 			return filteredList
 
-def main():
+def main(args):
 	logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 	# logging.basicConfig(filename='myapp.log', level=logging.INFO)
-	try:
-		filterby = sys.argv[2]
-	except IndexError:
-		filterby = None
-	flObj = FilterList(filterby)
-	lst = None
-	try:
-		lst = sys.argv[1]
-	except IndexError:
-		logging.info(" No List passed to filter")
-	if lst:
-		result = flObj.filterList(ast.literal_eval(lst))
-		if result:
-			logging.info(result)
+	result = None
+	filterby = args.filter
 
+	result = FilterList(filterby).filterList(eval(args.list))
+	if result:
+		logging.info(result)
+		
 
 if __name__ == "__main__":
 	main()
--- scripts/filterList.command
+++ scripts/filterList.command
@@ -1,5 +1,2 @@
-#!/usr/bin/env python
 
-import sys;
-sys.path.append("/Users/sanjeevkumar/Development/python/listFilter/python")
-print sys.argv
+# /usr/bin/env python -c "import os; print os.chdir('/Users/sanjeevkumar/Development/python/listFilter/python/'); import filterList; filterList.main();" $*
